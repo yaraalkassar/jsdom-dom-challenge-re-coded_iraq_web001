a24b5b2a9f0a6a5f023ab09abd0a97ec2c70d08c
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-27 05:29:36 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -3,7 +3,8 @@ let c=0;
 let timerState=1; // 1 for OFF
 let inc=document.getElementById('plus');
 let dec=document.getElementById('minus');
-let run = setTimeout(counterClock, 1000);
+
+let checkInterval = counterClock();
 window.addEventListener("DOMContentLoaded", counterClock);
 function counterClock() {
   let c = parseInt(counter.innerText)+1;
