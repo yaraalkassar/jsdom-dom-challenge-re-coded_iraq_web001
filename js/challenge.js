480d4c8bd6d03b6889c57fdcf1baa43f26416c5a
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-27 05:08:19 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -30,12 +30,10 @@ let pause= document.getElementById('pause');
 pause.addEventListener('click', stop)
 function stop() {
 let pauseText = pause.value;
-  if(pauseText==" pause "){
+  
     pause.innerHTML=" resume ";
-  }
-  else {
-    pause.innerHTML=" pause ";
-  }
+  
+
 
 }
 
